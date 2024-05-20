75fee736da49efdc2e54b3965e76617dc295104a
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-05-20 12:05:33 +0400

    Handle OrderExecuted event

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,7 +1,9 @@
 import {
   ExchangeContract,
   ExchangeContract_type_id_12,
+  ExchangeContract_type_id_21,
   makerOrderEntity,
+  takerOrderEntity,
 } from "generated";
 import { nanoid } from "nanoid";
 
@@ -32,6 +34,22 @@ function decodeMarketOrder(
   };
 }
 
+function decodeTakerOrder(
+  eventOrder: ExchangeContract_type_id_21
+): takerOrderEntity {
+  return {
+    id: nanoid(),
+    taker: eventOrder.taker.value,
+    side: eventOrder.side.case,
+    maker: eventOrder.maker.value,
+    collection: eventOrder.collection.value,
+    token_id: eventOrder.token_id,
+    price: eventOrder.price,
+    nonce: eventOrder.nonce,
+    strategy: eventOrder.strategy.value,
+  };
+}
+
 ExchangeContract.OrderPlaced.loader(({ event, context }) => {});
 
 ExchangeContract.OrderPlaced.handler(({ event, context }) => {
@@ -52,4 +70,15 @@ ExchangeContract.OrderUpdated.handler(({ event, context }) => {
     order_id: makerOrder.id,
   });
   context.MakerOrder.set(makerOrder);
+});
+
+ExchangeContract.OrderExecuted.loader(({ event, context }) => {});
+
+ExchangeContract.OrderExecuted.handler(({ event, context }) => {
+  const takerOrder = decodeTakerOrder(event.data.order);
+  context.OrderExecuted.set({
+    id: nanoid(),
+    order_id: takerOrder.id,
+  });
+  context.TakerOrder.set(takerOrder);
 });
