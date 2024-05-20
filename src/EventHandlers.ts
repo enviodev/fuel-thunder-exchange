ac48ad29a747cd445508e83b54fea4df49a80c47
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-05-20 12:00:16 +0400

    Handle OrderUpdated event

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,4 +1,8 @@
-import { ExchangeContract, makerOrderEntity } from "generated";
+import {
+  ExchangeContract,
+  ExchangeContract_type_id_12,
+  makerOrderEntity,
+} from "generated";
 import { nanoid } from "nanoid";
 
 function tai64ToDate(tai64: bigint) {
@@ -9,26 +13,41 @@ function tai64ToDate(tai64: bigint) {
   return new Date(+dateStr).toUTCString();
 }
 
+function decodeMarketOrder(
+  eventOrder: ExchangeContract_type_id_12
+): makerOrderEntity {
+  return {
+    id: nanoid(),
+    side: eventOrder.side.case,
+    maker: eventOrder.maker.value,
+    collection: eventOrder.collection.value,
+    token_id: eventOrder.token_id,
+    price: eventOrder.price,
+    amount: eventOrder.amount,
+    nonce: eventOrder.nonce,
+    strategy: eventOrder.strategy.value,
+    payment_asset: eventOrder.payment_asset.value,
+    start_time: tai64ToDate(eventOrder.start_time),
+    end_time: tai64ToDate(eventOrder.end_time),
+  };
+}
+
 ExchangeContract.OrderPlaced.loader(({ event, context }) => {});
 
 ExchangeContract.OrderPlaced.handler(({ event, context }) => {
-  const eOrder = event.data.order;
-  const makerOrder: makerOrderEntity = {
-    id: nanoid(),
-    side: eOrder.side.case,
-    maker: eOrder.maker.value,
-    collection: eOrder.collection.value,
-    token_id: eOrder.token_id,
-    price: eOrder.price,
-    amount: eOrder.amount,
-    nonce: eOrder.nonce,
-    strategy: eOrder.strategy.value,
-    payment_asset: eOrder.payment_asset.value,
-    start_time: tai64ToDate(eOrder.start_time),
-    end_time: tai64ToDate(eOrder.end_time),
-  };
-
+  const makerOrder = decodeMarketOrder(event.data.order);
   context.OrderPlaced.set({
+    id: nanoid(),
+    order_id: makerOrder.id,
+  });
+  context.MakerOrder.set(makerOrder);
+});
+
+ExchangeContract.OrderUpdated.loader(({ event, context }) => {});
+
+ExchangeContract.OrderUpdated.handler(({ event, context }) => {
+  const makerOrder = decodeMarketOrder(event.data.order);
+  context.OrderUpdated.set({
     id: nanoid(),
     order_id: makerOrder.id,
   });
